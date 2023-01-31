cb5a452508a5d443f8d95b42704cd59ae539c74d
Ajesh Vincent <ajeshvincent91@gmail.com>
2023-01-31 10:11:16 +0530

    Function Definitions Added

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Implementation file to perform various statistical operations
  *
- * <Add Extended Description Here>
+ * 
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Ajesh Vincent>
+ * @date <2023, January 30 >
  *
  */
 
@@ -37,7 +37,78 @@ void main() {
 
   /* Other Variable Declarations Go Here */
   /* Statistics and Printing Functions Go Here */
+  print_array(test,SIZE)
+  print_statistics(test,SIZE);
 
 }
 
 /* Add other Implementation File Code Here */
+void print_array(unsigned char test[], int N)
+{
+	for(int i=0;i<N;i++)
+	{
+		printf("%d\t",test[i]);
+	}
+	
+}
+void sort_array(unsigned char test[], int N)
+{
+	int i, j;
+	for (i = 0; i < N-1; i++) {
+		for (j = 0; j < N-i-1; j++) {
+		if (test[j] < test[j+1]) {
+        
+        char temp = test[j];
+        test[j] = test[j+1];
+        test[j+1] = temp;
+      }
+    }
+  }
+}
+double find_median(unsigned char test[], int N)
+{
+    //sort_array(test,N);
+    double median_data=(test[N/2]+test[(N/2)+1])/2;
+    return median_data;
+}
+double find_mean(unsigned char test[], int N)
+{
+    //sort_array(test,N);
+    int sum=0;
+    double mean_data=0.0;
+    for(int i=0;i<N;i++)
+    {
+        sum+=test[i];
+    }
+    mean_data=floor(sum/N);
+    return mean_data;
+}
+int find_maximum(unsigned char test[], int N)
+{
+    //sort_array(test,N);
+    return test[0];
+}
+int find_minimum(unsigned char test[], int N)
+{
+    //sort_array(test,N);
+    return test[N-1];
+}
+
+void print_statistics(unsigned char test[], int N)
+{
+		sort_array(test,N);
+		
+		double med=find_median(test,N);
+		printf("%f\t",med);
+		double me=find_mean(test,N);
+		printf("%f\t",me);
+		int max=find_maximum(test,N);
+		printf("%d\t",max);
+		int min=find_minimum(test,N);
+		printf("%d\t",min);
+		
+}
+
+
+
+
